221b199c0361924a996fcf3355c938fa384300bd
Karen McCulloch <k.a.z.mcc95@gmail.com>
2017-05-13 12:32:55 +0100

    set port using process environment variables.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-var port = 8080;
+var port = process.env.PORT || 8080;
 var bad_result = {"unix":null, "natural": null}
 module.exports =app 
 function formatNatDate(date){
